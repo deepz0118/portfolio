a28d0bc7c00165e40c0de23ba01fdcfb7e01932f
Deepika P <thangamdeepi49@gmail.com>
2024-09-21 01:26:37 -0700

    port

---
+++ port/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Content() {
+  return (
+    <div>
+
+        <h1>Hero, <br/> Im Deepika P
+        <p>Im a CSE(Cyber Security) Student</p>
+        </h1>
+
+    </div>
+  )
+}
---
+++ port/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SquareMenu } from 'lucide-react';
+import { SquareUserRound } from 'lucide-react';
+import { useState } from 'react';
+
+export default function Header() {
+    const [Menu,setMenu]=useState(false);
+  return (
+    <h1 className='flex justify-between px-5 bg-primary text-teal-300  font-bold'>
+    <a href='#'>Deepika P</a>
+    <nav className='hidden md:block'>
+    <ul className='flex  font-medium'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Skills</a></li>
+        <li><a href='#'>Projects</a></li>
+        <li><a href='#'>Contact</a></li>
+        <button><SquareUserRound className='text-teal-300 h-5'/></button>
+        </ul>    
+    </nav>
+    {Menu && <nav className='block md:hidden'>
+    <ul className='flex flex-col font-medium fixed top-10 w-full h-1/3 bg-black left-0 text-center border-b-2'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Skills</a></li>
+        <li><a href='#'>Projects</a></li>
+        <li><a href='#'>Contact</a></li>
+        <li><a href='#'>Login</a></li>
+        </ul>
+        </nav>}
+    <button onClick={()=>setMenu(!Menu)}className='block md:hidden'><SquareMenu className='text-teal-300 h-5 right-2'/></button>
+    </h1>
+  )
+}
